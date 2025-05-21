db982f20777a84f3df4182455033f91779284bfb
Xenos F. <git@xenosfio.com>
2025-05-21 22:09:50 +0800

    Remove unused code

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -8,10 +8,6 @@ void draw_animation(lv_obj_t *canvas) {
 
     lv_obj_t *art = lv_img_create(canvas);
 
-    int length = sizeof(anim_imgs) / sizeof(anim_imgs[0]);
-    srand(k_uptime_get_32());
-    int random_index = rand() % length;
-
     lv_img_set_src(art, &pixel_sq);
 
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
